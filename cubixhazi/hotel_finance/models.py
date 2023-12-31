664de7b8bdae8ea70795a992baed3660549fab2a
supernauth <helitamas@gmail.com>
2023-12-31 13:13:42 +0100

    correcting models, building templates

--- cubixhazi/hotel_finance/models.py
+++ cubixhazi/hotel_finance/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Type(models.Model):
     name = models.CharField(max_length=100)
     suite = models.BooleanField()
+    price = models.FloatField()
     
     def __str__(self):
         return f'{self.name}'
