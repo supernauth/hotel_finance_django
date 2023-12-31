36f350db60830dafedaf66ee3e5cf71e17c170fe
supernauth <helitamas@gmail.com>
2023-12-31 13:47:28 +0100

    site constructing

--- cubixhazi/cubixhazi/settings.py
+++ cubixhazi/cubixhazi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hotel_finance.apps.HotelFinanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
